4b72e4b1ab7dae42735b3339b2638593fb3f0ae2
Ben Keener <keener4christ@gmail.com>
2020-09-06 20:51:36 -0400

    Added spawn box for timer

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject TimerText;
+
+    private Timer timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = TimerText.GetComponent<Timer>();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        timer.enabled = true;
+    }
+}
