b6afccb5207ddfa713a59e382b1b9beb73d04948
Ben Keener <keener4christ@gmail.com>
2020-09-07 14:23:57 -0400

    Added timer stop when colliding with win flag

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject TextObj;
+    public GameObject PlayerObj;
+
+    private Text text;
+    private Timer timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = TextObj.GetComponent<Text>();
+        timer = PlayerObj.GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        timer.enabled = false;
+        text.fontSize = 60;
+        text.color = Color.green;
+    }
+}
