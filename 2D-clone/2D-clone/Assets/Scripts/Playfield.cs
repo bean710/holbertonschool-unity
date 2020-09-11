6ece703a56799dcfafb5a482bd09e763b0b40f16
Ben Keener <keener4christ@gmail.com>
2020-09-11 15:39:18 -0400

    Updated textures

---
+++ 2D-clone/2D-clone/Assets/Scripts/Playfield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playfield : MonoBehaviour
+{
+    public static int w = 10;
+    public static int h = 20;
+    public static Transform[,] grid = new Transform[w, h];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Rounds a 2D vector (position) to the nearest whole coordinates
+    public static Vector2 RoundV2(Vector2 vector)
+    {
+        return new Vector2(Mathf.Round(vector.x), Mathf.Round(vector.y));
+    }
+}
--- 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
+++ 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    public float speed = 1;
+
+    private float fall = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class Tetromino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - fall >= speed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+        }
     }
 }
