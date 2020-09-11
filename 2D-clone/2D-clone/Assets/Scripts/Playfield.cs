7d9a3d8c6a27fc6692e4783e67fa8805af20541f
Ben Keener <keener4christ@gmail.com>
2020-09-11 16:54:33 -0400

    Added fall collision

--- 2D-clone/2D-clone/Assets/Scripts/Playfield.cs
+++ 2D-clone/2D-clone/Assets/Scripts/Playfield.cs
@@ -6,7 +6,7 @@ public class Playfield : MonoBehaviour
 {
     public static int w = 10;
     public static int h = 20;
-    public static Transform[,] grid = new Transform[w, h];
+    public static Transform[,] playspace = new Transform[w, h];
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,41 @@ public class Playfield : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static void AddToPlayspace(Transform tm)
+    {
+        Vector2 r;
+
+        foreach (Transform child in tm)
+        {
+            r = RoundV2(child.position);
+            playspace[(int)r.x, (int)r.y] = child;
+        }
+    }
+
+    public static bool CheckPos(Transform tm) {
+        Vector2 r;
+
+        foreach (Transform child in tm)
+        {
+            r = RoundV2(child.position);
+            if (!inField(r) || playspace[(int)r.x, (int)r.y] != null)
+            {
+                Debug.Log("Invalid spot");;
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Checks if a position is in the field
+    public static bool inField(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 &&
+                (int)pos.x < w &&
+                (int)pos.y >= 0);
     }
 
     // Rounds a 2D vector (position) to the nearest whole coordinates
--- 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
+++ 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
@@ -7,6 +7,7 @@ public class Tetromino : MonoBehaviour
     public float speed = 1;
 
     private float fall = 0;
+    private bool moving = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,19 @@ public class Tetromino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - fall >= speed)
+        if (moving && Time.time - fall >= speed)
         {
+            Vector3 oldPos = transform.position;
+
             transform.position += new Vector3(0, -1, 0);
+
+            if (!Playfield.CheckPos(transform))
+            {
+                transform.position = oldPos;
+                Playfield.AddToPlayspace(transform);
+                moving = false;
+            }
+
             fall = Time.time;
         }
     }
