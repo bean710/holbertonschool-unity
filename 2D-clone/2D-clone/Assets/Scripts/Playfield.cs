0fbe3a080c207f78c44a4f39054537ac6eff0dd7
Ben Keener <keener4christ@gmail.com>
2020-09-12 00:08:52 -0400

    Added ghost

--- 2D-clone/2D-clone/Assets/Scripts/Playfield.cs
+++ 2D-clone/2D-clone/Assets/Scripts/Playfield.cs
@@ -8,6 +8,14 @@ public class Playfield : MonoBehaviour
     public static int h = 20;
     public static Transform[,] playspace = new Transform[w, h];
 
+    public Tetromino[] Tetrominos;
+
+    private static bool needNew = true;
+    private static bool gameOver = false;
+
+    private static List<int> bag = new List<int>();
+    private static Tetromino upNext = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +25,106 @@ public class Playfield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!gameOver && needNew)
+        {
+            NewTetrimino();
+            needNew = false;
+        }
     }
 
+    // Get new tetrimino
+    private void NewTetrimino()
+    {
+        int bagIndex, index;
+
+        if (upNext == null)
+        {
+            if (bag.Count == 0)
+            {
+                for (int i = 0; i < Tetrominos.Length; i++)
+                    bag.Add(i);
+            }
+
+            bagIndex = (int)Random.Range(0, bag.Count);
+            index = bag[bagIndex];
+            bag.RemoveAt(bagIndex);
+            upNext = Instantiate(Tetrominos[index], new Vector3(-10, 14, 0), Quaternion.identity);
+        }
+
+        upNext.transform.position = new Vector3(5, 17, 0);
+        upNext.Enable();
+        if (!CheckPos(upNext.transform))
+        {
+            Destroy(upNext.transform.gameObject);
+            gameOver = true;
+        }
+
+        if (bag.Count == 0)
+        {
+            for (int i = 0; i < Tetrominos.Length; i++)
+                bag.Add(i);
+        }
+
+        bagIndex = (int)Random.Range(0, bag.Count);
+        index = bag[bagIndex];
+        bag.RemoveAt(bagIndex);
+
+        upNext = Instantiate(Tetrominos[index], new Vector3(-10, 14, 0), Quaternion.identity);
+    }
+
+    // Checks for a full row in the playspace and removes full rows
+    public static void CheckRows()
+    {
+        for (int i = 0; i < h; i++)
+        {
+            bool empty = false;
+            for (int j = 0; j < w; j++)
+            {
+                if (playspace[j, i] == null)
+                {
+                    empty = true;
+                    break;
+                }
+            }
+
+            if (!empty)
+            {
+                //Debug.Log("Found full row!");
+                for (int y = i; y < h; y++)
+                {
+                    bool nonEmpty = false;
+
+                    for (int x = 0; x < w; x++)
+                    {
+                        if (playspace[x, y] != null)
+                        {
+                            if (y == i)
+                                Destroy(playspace[x, y].gameObject);
+                        }
+                        /**
+                        else
+                            nonEmpty = true;
+                        **/
+
+                        if (y + 1 < h)
+                            playspace[x, y] = playspace[x, y + 1];
+                        else
+                            playspace[x, y] = null;
+
+                        if (playspace[x, y] != null)
+                            playspace[x, y].position += new Vector3(0, -1, 0);
+                    }
+
+                    if (nonEmpty)
+                        break;
+                }
+
+                i--;
+            }
+        }
+    }
+
+    // Adds a Tetromino to the playspace array
     public static void AddToPlayspace(Transform tm)
     {
         Vector2 r;
@@ -29,8 +134,12 @@ public class Playfield : MonoBehaviour
             r = RoundV2(child.position);
             playspace[(int)r.x, (int)r.y] = child;
         }
+
+        CheckRows();
+        needNew = true;
     }
 
+    // Checks a Tetromino against the playspace and boundaries
     public static bool CheckPos(Transform tm) {
         Vector2 r;
 
@@ -39,7 +148,7 @@ public class Playfield : MonoBehaviour
             r = RoundV2(child.position);
             if (!inField(r) || playspace[(int)r.x, (int)r.y] != null)
             {
-                Debug.Log("Invalid spot");;
+                //Debug.Log("Invalid spot");;
                 return false;
             }
         }
--- 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
+++ 2D-clone/2D-clone/Assets/Scripts/Tetromino.cs
@@ -5,33 +5,151 @@ using UnityEngine;
 public class Tetromino : MonoBehaviour
 {
     public float speed = 1;
-
+    public bool canRotate = true;
+    public bool isGhost = false;
+    
     private float fall = 0;
-    private bool moving = true;
+    private bool moving = false;
+
+    private Tetromino ghost = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!isGhost)
+        {
+            UpdateGhost();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moving && Time.time - fall >= speed)
+        if (moving && !isGhost)
         {
-            Vector3 oldPos = transform.position;
-
-            transform.position += new Vector3(0, -1, 0);
-
-            if (!Playfield.CheckPos(transform))
+            if (Time.time - fall >= 1 / speed)
             {
+                Vector3 oldPos = transform.position;
+
+                transform.position += new Vector3(0, -1, 0);
+
+                if (!Playfield.CheckPos(transform))
+                {
+                    transform.position = oldPos;
+                    Playfield.AddToPlayspace(transform);
+                    moving = false;
+
+                    Destroy(ghost.transform.gameObject);
+                    return;
+                }
+
+                fall = Time.time;
+            }
+
+            if (Input.GetKeyDown("left"))
+            {
+                Vector3 oldPos = transform.position;
+
+                transform.position += new Vector3(-1, 0, 0);
+
+                if (!Playfield.CheckPos(transform))
+                {
+                    transform.position = oldPos;
+                }
+
+                UpdateGhost();
+            }
+            else if (Input.GetKeyDown("right"))
+            {
+                Vector3 oldPos = transform.position;
+
+                transform.position += new Vector3(1, 0, 0);
+
+                if (!Playfield.CheckPos(transform))
+                {
+                    transform.position = oldPos;
+                }
+
+                UpdateGhost();
+            }
+            else if (Input.GetKeyDown("space"))
+            {
+                Vector3 oldPos = transform.position;
+
+                while (Playfield.CheckPos(transform))
+                {
+                    oldPos = transform.position;
+                    transform.position += new Vector3(0, -1, 0);
+                }
+
                 transform.position = oldPos;
                 Playfield.AddToPlayspace(transform);
                 moving = false;
+
+                Destroy(ghost.transform.gameObject);
+                return;
+            }
+            else if (canRotate && Input.GetKeyDown("up"))
+            {
+                transform.Rotate(0, 0, 90);
+
+                if (!Playfield.CheckPos(transform))
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+                else
+                {
+                    foreach (Transform child in transform)
+                    {
+                        child.Rotate(0, 0, -90);
+                    }
+
+                    UpdateGhost();
+                }
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                speed *= 2;
             }
 
-            fall = Time.time;
+            if (Input.GetKeyUp("down"))
+            {
+                speed /= 2;
+            }
         }
+    }
+
+    public void UpdateGhost()
+    {
+        Debug.Log($"Updating as ghost: {(ghost ? "true" : "false")}");
+        if (moving)
+        {
+            if (ghost == null)
+            {
+                Debug.Log("New ghost");
+                ghost = Instantiate(this, transform.position, transform.rotation);
+                ghost.isGhost = true;
+                foreach (Transform child in ghost.transform)
+                    child.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+            }
+
+            ghost.transform.position = transform.position;
+            ghost.transform.rotation = transform.rotation;
+            Vector3 oldPos = ghost.transform.position;
+
+            while (Playfield.CheckPos(ghost.transform))
+            {
+                oldPos = ghost.transform.position;
+                ghost.transform.position += new Vector3(0, -1, 0);
+            }
+
+            ghost.transform.position = oldPos;
+        }
+    }
+
+    public void Enable()
+    {
+        moving = true;
+        UpdateGhost();
     }
 }
