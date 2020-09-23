2a1488a5024060d474abe9ac212484e292fee48d
Ben Keener <keener4christ@gmail.com>
2020-09-23 15:50:59 -0400

    Completed task 4

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public List<string> levelNames;
+
+    private SceneChanger sc;
+    public void LevelSelect(int level)
+    {
+        SceneChanger.ChangeScene(levelNames[level - 1]);
+    }
+
+    public void Options()
+    {
+        SceneChanger.ChangeScene("Options");
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    private SceneChanger sc;
+
+    public void Back()
+    {
+        SceneChanger.LastScene();
+    }
+}
