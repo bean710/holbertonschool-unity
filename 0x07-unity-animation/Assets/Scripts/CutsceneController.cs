fde35cec46258373ec66d19bc84dc2d9260a11d0
Ben Keener <keener4christ@gmail.com>
2020-09-29 14:23:27 -0400

    Completed task 2

--- 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CutsceneController : MonoBehaviour
 {
+    public GameObject MainCamera;
+    public GameObject Player;
+    public GameObject TimerCanvas;
+
+    private PlayerController pc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pc = Player.GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void AnimDone()
     {
-        
+        MainCamera.SetActive(true);
+        TimerCanvas.SetActive(true);
+        pc.enabled = true;
+        transform.gameObject.SetActive(false);
     }
 }
