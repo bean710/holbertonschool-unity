31145c58200f66586ee6d83b916b462fd8d3f068
Ben Keener <keener4christ@gmail.com>
2020-10-19 18:51:32 -0400

    Added hover audio to buttons

---
+++ 0x08-unity-audio/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonAudio : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource hover;
+
+    public void MouseEnter() {
+        Debug.Log("Foo");
+        hover.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hover.Play();
+    }
+}
